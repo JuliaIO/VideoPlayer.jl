f42cc6f4e91997fd567b798672a3fe8bc8efe336
yakir12 <12.yakir@gmail.com>
2019-07-10 15:14:41 +0200

    added a slider

--- src/makie.jl
+++ src/makie.jl
@@ -1,17 +1,17 @@
 using Makie, VideoIO, Dates, Observables
 avf = VideoIO.testvideo("annie_oakley")
 f = VideoIO.openvideo(avf)
+seek(f, 5.0)
 img = read(f)
 sz = size(img)
-nframesbuffer = round(Int, 5f.framerate)
+nframesbuffer = round(Int, 15f.framerate)
 buff = Channel{typeof(img)}(nframesbuffer)
-function fillbuff()
-    while !eof(f) 
+task = @async begin
+    while !eof(f)
         put!(buff, read(f))
     end
 end
-task = @async fillbuff()
-bind(buff, task)
+# bind(buff, task)
 flipx=false
 flipy=false
 pixelaspectratio = VideoIO.aspect_ratio(f)
@@ -27,16 +27,35 @@ else
     flipy && Makie.scale!(scene, 1, -1, 1)
 end
 next_button = button(">", raw=true, camera=campixel!) 
-function readshow(_ = nothing)
+function readshow(i = nothing)
     makieimg[3] = take!(buff)
 end
 stepped = lift(readshow, next_button[end][:clicks])
+nframes = round(Int, VideoIO.get_duration(avf.io)/Microsecond(Second(1))*f.framerate)
+slider_h = slider(1:nframes, raw = true, camera = campixel!, start = 2)
+old_slider_pos = Node(1)
+lift(slider_h[end][:value]) do frame
+    @show frame old_slider_pos[] buff
+    if old_slider_pos[] ≤ frame# < nframesbuffer
+        for i in old_slider_pos[]:frame# - old_slider_pos[]
+            take!(buff)
+        end
+        readshow()
+        old_slider_pos[] = frame
+    else
+        @show frame
+    end
+end
+hbox(vbox(slider_h, next_button), scene)
+
 play_button = button("▷", raw=true, camera=campixel!) 
 function play(c)
+    println("play: ", c)
     if isodd(c)
         for img in buff
             makieimg[3] = img
             sleep(1/f.framerate)
+            break
         end
     end
 end
