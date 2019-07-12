2ff4e5019cd080217b7f00822bcbee17d2c5bd86
yakir12 <12.yakir@gmail.com>
2019-07-12 23:23:57 +0200

    makie3 works

--- src/testvideo.jl
+++ src/testvideo.jl
@@ -1,10 +1,9 @@
 function createtestvideo(filename; w = 200, h = 100, fps = 25, totalseconds = 10)
-
     totalframes = fps*totalseconds
     Δt = Millisecond(Second(1))/fps
     framenumber = Observable(1)
     msg = lift(framenumber) do fn
-        t = Time(0) + fn*Δt
+        t = Time(0) + (fn - 1)*Δt
         """
         Test video
         Frame #: $fn
@@ -13,7 +12,6 @@ function createtestvideo(filename; w = 200, h = 100, fps = 25, totalseconds = 10
     scene = Scene(resolution = (w, h), backgroundcolor = :black)
     text!(scene, msg, color = :white)
     # scene
-
     record(scene, filename, 1:totalframes, framerate = fps) do i
         framenumber[] = i
     end
